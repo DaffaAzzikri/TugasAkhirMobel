d424fc9785868423036757a997ebc26c6d21f59d
RizqanKarim <2410817110005@mhs.ulm.ac.id>
2026-06-11 22:06:45 +0800

    Menambahkan plugins dan depedency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
     id("com.google.gms.google-services")
     id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -78,4 +79,8 @@ dependencies {
     implementation("com.cloudinary:cloudinary-android:2.5.0")
 
     implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("com.google.dagger:hilt-android:2.51")
+    kapt("com.google.dagger:hilt-compiler:2.51")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 }
