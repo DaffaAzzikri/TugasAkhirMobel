59b5940d559697995f594e60480646eaa7a78c3b
DaffaAzzikri <daffaazzikrii@gmail.com>
2026-06-13 23:57:41 +0800

    Logika minimum stok

--- app/src/main/java/com/example/tugasakhirmobel/data/remote/model/BarangListResponse.kt
+++ app/src/main/java/com/example/tugasakhirmobel/data/remote/model/BarangListResponse.kt
@@ -7,16 +7,3 @@ data class BarangListResponse(
     @SerializedName("message") val message: String,
     @SerializedName("data") val data: List<BarangModel>
 )
-
-// Struktur data barang yang sudah diperbarui sesuai FastAPI
-data class BarangModel(
-    @SerializedName("id") val id: Int?,
-    @SerializedName("nama_barang") val namaBarang: String,
-    @SerializedName("sku") val sku: String,             // TAMBAHAN BARU
-    @SerializedName("kategori") val kategori: String,   // TAMBAHAN BARU
-    @SerializedName("harga") val harga: Int,            // TAMBAHAN BARU
-    @SerializedName("supplier") val supplier: String,   // TAMBAHAN BARU
-    @SerializedName("stok") val stok: Int,
-    @SerializedName("image_url") val imageUrl: String,
-    @SerializedName("uid_pembuat") val uidPembuat: String?
-)
